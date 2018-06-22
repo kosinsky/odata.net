bc5d3a7c66e9de43d37b050ba9458072edefb47f
Konstantin Kosinsky <kokosins@microsoft.com>
2018-06-22 14:45:05 -0700

    Tune init.cmd

--- init.cmd
+++ init.cmd
@@ -1,10 +1,27 @@
 PUSHD %~dp0
 set BUILDDIR=%CD%
-set PACKAGES = %BUILDDIR%\sln\packages
+
+rem set toolsets next to where we are
+call :SetupToolsDir %BUILDDIR%\sln\packages
+
 rem set MSBuildFrameworkToolsPath
 set MSBuildFrameworkToolsPath=%windir%\Microsoft.Net\Framework\v4.0.30319
 
 rem add it to the PATH, too
 set PATH=%MSBuildFrameworkToolsPath%;%PATH%
 
-set MSBuildLocation=%PACKAGES%\Msbuild.Corext.15.5.27130\v15.0\bin\msbuild.exe
+rem set MSBuildLocation
+set MSBuildLocation=%TOOLSDIR%\Msbuild.Corext.15.5.27130\v15.0\bin\msbuild.exe
+
+rem now add msbuild dir
+set PATH=%TOOLSDIR%\Msbuild.Corext.15.5.27130\v15.0\bin;%PATH%
+
+rem add csc.exe dir
+set PATH=%TOOLSDIR%\Msbuild.Corext.15.5.27130\v15.0\bin\Roslyn;%PATH%
+
+exit
+
+:SetupToolsDir
+set TOOLSDIR=%~f1
+if not exist %TOOLSDIR% mkdir %TOOLSDIR%
+exit /b 0
