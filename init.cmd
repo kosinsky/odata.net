78cd5522f8e68088ffbf90bb5bcd102a0fb233c7
Konstantin Kosinsky <kokosins@microsoft.com>
2018-06-22 13:26:55 -0700

    Tune init.cmd

--- init.cmd
+++ init.cmd
@@ -1,10 +1,10 @@
-set Root = %~dp0
-set Packages = %Root%\sln\packages
+PUSHD %~dp0
+set BUILDDIR=%CD%
+set PACKAGES = %BUILDDIR%\sln\packages
 rem set MSBuildFrameworkToolsPath
 set MSBuildFrameworkToolsPath=%windir%\Microsoft.Net\Framework\v4.0.30319
 
 rem add it to the PATH, too
 set PATH=%MSBuildFrameworkToolsPath%;%PATH%
 
-move %Packages%\Msbuild.Corext.*\ %Packages%\Msbuild.Corext\
-set MSBuildLocation=%Packages%\Msbuild.Corext\v15.0\bin\msbuild.exe
+set MSBuildLocation=%PACKAGES%\Msbuild.Corext.15.5.27130\v15.0\bin\msbuild.exe
