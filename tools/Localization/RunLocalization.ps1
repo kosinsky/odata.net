bbffa0d2d6c8c1f16791c3cf76eff8a1afbe674f
Konstantin Kosinsky <kokosins@microsoft.com>
2018-06-28 16:42:14 -0700

    Rename lcl files

--- tools/Localization/RunLocalization.ps1
+++ tools/Localization/RunLocalization.ps1
@@ -33,7 +33,7 @@ $Locales = @{1041="ja-JP";
 ForEach($dll in $ProductDlls)
 {
     Write-Host "Localizing $dll ..."
-    $originalDll = $dll.Replace("TeamFoundation.","")
+    $originalDll = $dll#.Replace("TeamFoundation.","")
     $resourceDll = $dll.Replace(".dll",".resource.dll")
     & $LSBuild parse /l en-US /w 2 /p 211 /we /s $LSSPATH /vc $LCIPath\$originalDll.lci /o $BinariesPath\en-US\$dll.lcg  $BinariesPath\$dll
 
