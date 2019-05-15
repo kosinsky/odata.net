116e15b8f9eb586c99b63dafd02304ef55600158
Konstantin Kosinsky <kokosins@microsoft.com>
2019-05-15 15:25:53 -0700

    More properties exposed in IFunctionCallNode

--- src/Microsoft.OData.Core/UriParser/SemanticAst/IFunctionCallNode.cs
+++ src/Microsoft.OData.Core/UriParser/SemanticAst/IFunctionCallNode.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.OData.Edm;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,12 @@ namespace Microsoft.OData.UriParser
 {
     public interface IFunctionCallNode
     {
+        IEnumerable<IEdmFunction> Functions { get; }
+
         IEnumerable<QueryNode> Parameters { get; }
+
+        string Name { get; }
+
+        QueryNode Source { get; }
     }
 }
